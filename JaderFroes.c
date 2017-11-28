e39817d9511adaadd14e1ba91072ab25fa565c7c
jaderfroes <jfdsrosa@inf.ufpel.edu.br>
2017-11-27 22:16:08 -0200

    Versao final sem bugs

--- JaderFroes.c
+++ JaderFroes.c
@@ -14,20 +14,28 @@ struct vetor{
 	dado **lista;
 	int tamanho, totalDado;
 };
+typedef struct palavraCorreta sugestao;
+struct palavraCorreta{//struct que tera as referencias das sugestoes das palavras incorretas
+	char *palavra;
+	sugestao *prox;
+};
 
 
-hash *initiHash(hash *dicionario);
-hash *fatorCarga(hash *dicionario);
-hash *rehash(hash *dicionario);
-int buscaPalavra(hash *dicionario, char consulta[], int valorAscii);
-void adicionaPalavra(hash *dicionario, char novaPalavra[], int valorAscii);
-void deletaPalavra(hash *dicionario, char consulta[], int valorAscii);
-void reconhecimento(hash *dicionario,char entrada[],int valorAscii);
-int letraMais(hash *dicionario, char consulta[], int valorAscii);
-int letraMenos(hash *dicionario, char consulta[], int valorAscii);
-int letrasVizinhas(hash *dicionario, char consulta[], int valorAscii);
-int umaLetraErrada(hash *dicionario, char consulta[], int valorAscii);
-hash* destroiHash(hash *dicionario);
+hash *initiHash(hash *dicionario); //inicializa hash
+hash *fatorCarga(hash *dicionario); //verifica fator de carga
+hash *rehash(hash *dicionario); //faz rehash
+int buscaPalavra(hash *dicionario, char consulta[], int valorAscii); //busca a palavra e retorna V ou F
+dado *buscaEndPalavra(hash *dicionario, char consulta[], int valorAscii); //busca a palavra e retorna o endereco do nodo
+void adicionaPalavra(hash *dicionario, char novaPalavra[], int valorAscii); //adiciona 
+void deletaPalavra(hash *dicionario, char consulta[], int valorAscii); //delete
+void reconhecimento(hash *dicionario,char entrada[],int valorAscii); //chama os algoritmos de reconhecimento de palavras incorretas
+sugestao *letraMais(hash *dicionario, char consulta[], int valorAscii, sugestao *sugestoes);
+sugestao * letraMenos(hash *dicionario, char consulta[], int valorAscii, sugestao *sugestoes);
+sugestao * letrasVizinhas(hash *dicionario, char consulta[], int valorAscii, sugestao *sugestoes);
+sugestao * umaLetraErrada(hash *dicionario, char consulta[], int valorAscii, sugestao *sugestoes);
+sugestao *adicionaSugestoes(dado *nodoHash, sugestao* sugestoes); //cria uma lista de sugestoes em ordem alfabetica
+sugestao *printaListaEDeleta(sugestao *sugestoes); //imprime a lista anteriormente criada e deleta os elementos
+hash *destroiHash(hash *dicionario);
 
 int main(void){
 	char entrada[50], anterior[50];
@@ -150,6 +158,27 @@ int buscaPalavra(hash *dicionario, char consulta[], int valorAscii){
 	return 0;
 }
 
+dado *buscaEndPalavra(hash *dicionario, char consulta[], int valorAscii){
+	int posicao = valorAscii % dicionario->tamanho;
+	if (valorAscii < 0)
+		return NULL;
+
+	if(dicionario->lista[posicao] == NULL){
+		return NULL;
+	}
+	else{
+		dado *aux = dicionario->lista[posicao];
+		do{
+			//printf("eh o atual: %s\n", aux->palavra);
+			if(!strcmp(consulta, aux->palavra)){
+				return aux;
+			}
+			aux = aux->prox;
+		}while(aux != NULL);
+	}
+	return NULL;
+}
+
 void adicionaPalavra(hash *dicionario, char novaPalavra[], int valorAscii){
 	int posicao = valorAscii % dicionario->tamanho;
 	dado *new = NULL;
@@ -200,25 +229,30 @@ void deletaPalavra(hash *dicionario, char consulta[], int valorAscii){
 }
 
 void reconhecimento(hash *dicionario, char entrada[],int valorAscii){
-	/*printf("rec = %d, %d, %d, %d\n", letraMais(dicionario,entrada, valorAscii), letraMenos(dicionario,entrada, valorAscii),
-	letrasVizinhas(dicionario,entrada, valorAscii), umaLetraErrada(dicionario,entrada, valorAscii));*/
+	sugestao *sugestoes=NULL; //Lista de ponteiros que apontarao para as palavras corretas sugeridas
 
-	if(letraMais(dicionario,entrada, valorAscii) || letraMenos(dicionario,entrada, valorAscii) || 
-	letrasVizinhas(dicionario,entrada, valorAscii) || umaLetraErrada(dicionario,entrada, valorAscii)){ //caso algum algoritmo encontre uma palavra parecida
+	sugestoes = letraMais(dicionario,entrada, valorAscii, sugestoes); 
+	sugestoes = letraMenos(dicionario,entrada, valorAscii, sugestoes);
+	sugestoes = letrasVizinhas(dicionario,entrada, valorAscii, sugestoes);
+	sugestoes = umaLetraErrada(dicionario,entrada, valorAscii, sugestoes); //caso algum algoritmo encontre uma palavra parecida
+
+	if (sugestoes != NULL){
+		// printf(" Antes do printa lista\n");
+		sugestoes = printaListaEDeleta(sugestoes);
 		printf("\n");
+		// printf(" Após o printa lista\n");
+
 	}
 	else{ //nenhuma palavra similar encontrada
 		printf("not found\n");
 	}
-
 	return;
 }
 
-int letraMais(hash *dicionario, char consulta[], int valorAscii){
+sugestao *letraMais(hash *dicionario, char consulta[], int valorAscii, sugestao *sugestoes){
 	char aux[50];
 	int k = 0; //posicao da letra que sera 'comida'
 	int j = 0; //descola na string auxiliar
-	int flag = 0;
 
 	do{
 		valorAscii -= consulta[k];
@@ -232,66 +266,59 @@ int letraMais(hash *dicionario, char consulta[], int valorAscii){
 		}
 		aux[j] = '\0';
 		if(buscaPalavra(dicionario, aux, valorAscii)){
-			printf("%s ",aux );
-			flag = 1;
+			sugestoes = adicionaSugestoes(buscaEndPalavra(dicionario, aux, valorAscii), sugestoes);
 		}
 		valorAscii+= consulta[k];
 		k++;
 	} while (consulta[k]!='\0');
-	return flag;
+	return sugestoes;
 }
 
-int letraMenos(hash *dicionario, char consulta[], int valorAscii){
+sugestao *letraMenos(hash *dicionario, char consulta[], int valorAscii, sugestao *sugestoes){
 	char troca;
-	char aux[50] = "a";
-	int i;
+	char aux[102] = "a";
 	int j;
-	int flag = 0;
-
-	strcat(aux, consulta); //concatena as strings "adicionando" uma letra
-
-	for (int i = 0; i < aux[i] != '\0'; ++i){//percorre a palavra
+	strcat(aux, consulta); //concatena as strings "adicionando" uma letraMenos
+	int i = 0;
+	do {
 		for (j = 97; j < 123; ++j){ //percorre o alfabeto
 			aux[i] = j;
 			valorAscii+=j;
 			if(buscaPalavra(dicionario, aux, valorAscii)){
-				printf("%s ", aux);
-				flag = 1;
+				sugestoes = adicionaSugestoes(buscaEndPalavra(dicionario, aux, valorAscii), sugestoes);
 			}
 			valorAscii-=j;
 		}
 		troca = aux[i];
 		aux[i] = aux[i+1];
 		aux[i+1] = troca;
-	}
+		i++;
+	} while (aux[i-1] != '\0');
 	
-	return flag;
+	return sugestoes;
 }
 
-int letrasVizinhas(hash *dicionario, char consulta[], int valorAscii){
+sugestao *letrasVizinhas(hash *dicionario, char consulta[], int valorAscii, sugestao *sugestoes){
 	char aux;
-	int flag = 0;
 
 	for (int i = 0; consulta[i] != '\0'; ++i){
 		aux = consulta[i];
 		consulta[i] = consulta[i+1];
 		consulta[i+1] = aux;
 		if(buscaPalavra(dicionario, consulta, valorAscii)){
-			printf("%s ", consulta);
-			flag = 1;
+			sugestoes = adicionaSugestoes(buscaEndPalavra(dicionario, consulta, valorAscii), sugestoes);
 		}
 		consulta[i+1] = consulta[i];
 		consulta[i] =aux;
 	}
 	
-	return flag;
+	return sugestoes;
 }
 
-int umaLetraErrada(hash *dicionario, char consulta[], int valorAscii){
+sugestao *umaLetraErrada(hash *dicionario, char consulta[], int valorAscii, sugestao *sugestoes){
 	char aux;
 	int i;
 	int j;
-	int flag = 0;
 	for (i= 0; consulta[i] != '\0'; ++i){ //percorre a palavra
 		aux = consulta[i]; //guardando a letra alterada
 		valorAscii-= consulta[i];
@@ -300,15 +327,63 @@ int umaLetraErrada(hash *dicionario, char consulta[], int valorAscii){
 			valorAscii+=j;
 			
 			if(buscaPalavra(dicionario, consulta, valorAscii)){
-				printf("%s ", consulta);
-				flag = 1;
+				sugestoes = adicionaSugestoes(buscaEndPalavra(dicionario, consulta, valorAscii), sugestoes);
 			}
 			valorAscii-=j;
 		}
 		valorAscii+= aux;
 		consulta[i] = aux;
 	}
-	return flag;
+	return sugestoes;
+}
+
+sugestao* adicionaSugestoes(dado *nodoHash, sugestao* sugestoes){
+	sugestao *nova;
+	nova = malloc(sizeof(sugestao));
+	nova->palavra = nodoHash->palavra;
+	if (sugestoes == NULL){
+		nova->prox = NULL;
+		sugestoes = nova;
+		return sugestoes;
+	}
+
+	sugestao *aux = sugestoes;
+	do{
+		if(!strcmp(nodoHash->palavra, aux->palavra) ){
+			return sugestoes;
+		}
+			for(int i = 0; nodoHash->palavra[i] != '\0' ; ++i){
+				if(nodoHash->palavra[i] < aux->palavra[i]){
+					nova->prox = sugestoes;
+					sugestoes = nova;
+					return sugestoes;
+				}
+				if(nodoHash->palavra[i] > aux->palavra[i]){
+					aux->prox = nova;
+					nova->prox = NULL;
+					return sugestoes;
+				}
+			}
+			aux = aux->prox;
+
+	}while(aux != NULL);
+
+	return sugestoes;
+}
+
+sugestao *printaListaEDeleta(sugestao *sugestoes){
+	sugestao *aux = sugestoes;
+	// printf("to AQUI \n");
+	
+	do{
+		printf("%s ", (aux->palavra));
+		sugestoes = aux->prox;
+		free(aux);
+		aux = sugestoes;
+	} while (aux!= NULL);
+
+	return sugestoes;
+
 }
 
 hash* destroiHash(hash *dicionario){
